e23f9e09c43cb6ab4ee59607bcdc00a6d340f98b
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2024-03-06 10:18:26 +0600

    Update 06-03.24.sh

--- 06-03.24.sh
+++ 06-03.24.sh
@@ -1,3 +1,5 @@
+SUM
+
 read -p "First number: " num1
 read -p "second number: " num210
 sum=$(($num1+$num2))
@@ -5,6 +7,10 @@ echo "sum:$sum"
 
 
 -----------------
+
+IF ELSE
+
+
 num1=10
 num2=20
 
